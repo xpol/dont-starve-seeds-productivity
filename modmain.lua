fc1275367619f0c67b897481f8278aac30d3b40f
Xpol Wan <xpolife@gmail.com>
2016-09-05 17:52:27 +0800

    Fixes use of GLOBAL.Prefabs.

--- modmain.lua
+++ modmain.lua
@@ -1,4 +1,4 @@
--- luacheck: globals GetModConfigData AddPrefabPostInit Prefabs
+-- luacheck: globals GetModConfigData AddPrefabPostInit GLOBAL
 
 local PRODUCTIVITY = GetModConfigData('PRODUCTIVITY')
 
@@ -6,7 +6,7 @@ AddPrefabPostInit('birdcage', function(inst)
 		local onaccept = inst.components.trader.onaccept
 		inst.components.trader.onaccept = function(instance, giver, item)
 				local seed_name = string.lower(item.prefab .. '_seeds')
-				local hasSeeds = Prefabs[seed_name]
+				local hasSeeds = GLOBAL.Prefabs[seed_name]
 				local times = hasSeeds and PRODUCTIVITY or 1
 				print(('Birdcage: eating %s with %d times output'):format(item.prefab, times))
 				for _=1,times do
